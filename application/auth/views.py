81b1c6c75921ddc64b6ecab58ff914cc232c0ec2
Aleksander <matikainen.aleksander@gmail.com>
2019-02-23 22:29:28 +0200

    Ulkoasun viilaus

--- application/auth/views.py
+++ application/auth/views.py
@@ -9,28 +9,28 @@ from flask_login import current_user
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
     if request.method == "GET":
-        return render_template("auth/loginform.html", form = LoginForm())
+        return render_template("auth/loginform.html", form = LoginForm(), error = None)
 
     form = LoginForm(request.form)
 
     user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
     if not user:
         return render_template("auth/loginform.html", form = form,
-                                error = "No such username or password")
+                                error = "Väärä käyttäjätunnus tai salasana. Yritä uudelleen.")
 
-
+    
     login_user(user)
     return redirect(url_for("index")) 
 
 @app.route("/auth/regestration", methods = ["GET", "POST"])
 def auth_registration():
     if request.method == "GET":
-        return render_template("auth/registrationform.html", form = RegistrationForm())
+        return render_template("auth/registrationform.html", form = RegistrationForm(), error = None)
                                                                     
     form = RegistrationForm(request.form)
 
     if not form.validate():
-        return render_template("auth/registrationform.html", form=form)
+        return render_template("auth/registrationform.html", form=form, error = None)
 
     user = User.query.filter_by(username=form.username.data).first()
     if not user:
@@ -40,7 +40,8 @@ def auth_registration():
         login_user(user2)
         return redirect(url_for("index")) 
     else: 
-        return render_template("auth/registrationform.html", form=form, error = "Username is already taken. Please try new one")
+        form
+        return render_template("auth/registrationform.html", form=form, error = "Haluamasi käyttäjätunnus on jo varattu. Kokeile toisen.")
 
     return redirect(url_for("index"))
 
@@ -62,4 +63,4 @@ def auth_saldo():
                                 error = "Sorry, something gone wrong")
     user.saldo = user.saldo + form.maara.data
     db.session().commit()
-    return redirect(url_for("index")) 
+    return redirect(url_for("auth_saldo"))  
